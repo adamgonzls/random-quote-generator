063976f3f54192f57b5318670b34c1ebe6cabb8c
Adam Gonzales <adamgonzales1@gmail.com>
2016-10-11 21:45:43 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp 					= require('gulp');
+var browserSync 	= require('browser-sync').create();
+var sass 					= require('gulp-sass');
+var autoprefixer 	= require('gulp-autoprefixer');
+
+// static server + watch scss/html files
+gulp.task('serve', ['sass'], function() {
+	browserSync.init({
+		server: "./"
+	});
+
+	gulp.watch("./sass/*.scss", ['sass']);
+	gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// compile sass & auto-inject into browsers
+
+//sass
+gulp.task('sass', function() {
+	return gulp.src('./sass/*.scss')
+	.pipe(sass().on('error', sass.logError))
+//pipe to autoprefixer
+	.pipe(autoprefixer({
+		browsers: ['last 5 versions'],
+		cascade: false
+	}))
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
