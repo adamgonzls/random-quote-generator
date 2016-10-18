252fb118c88d62446c8e75ac6a9d81f101a4267c
Adam Gonzales <adamgonzales1@gmail.com>
2016-10-17 22:49:56 -0700

    add copy

--- js/index.js
+++ js/index.js
@@ -28,12 +28,35 @@ var quotes = [
 		quote: "Where all think alike, no one thinks very much.",
 		author: "Walter Lippmann", 
 		profession: "writer and journalist"
+	},
+	{
+		quote: "Whenever the world throws rose petals at you, which thrill and seduce the ego, beware.",
+		author: "Anne Lamott",
+		profession: "American writer"
+	},
+	{
+		quote: "One of the things I learned the hard way was that it doesn't pay to get discouraged. Keeping busy and making optimism a way of life can restore your faith in yourself.",
+		author: "Lucille Ball",
+		profession: "American actress and comedienne"
+	},
+	{
+		quote: "The unfed mind devours itself.",
+		author: "Gore Vidal",
+		profession: "American Writer"
+	},
+	{
+		quote: "Winning is not a sometime thing; it’s an all time thing. You don’t win once in a while, you don’t do things right once in a while, you do them right all the time. Winning is a habit, Unfortunately, so is losing.",
+		author: "Vince Lombardi"
+	},
+	{
+		quote: "Inaction Breeds Doubt And Fear. Action Breeds Confidence And Courage. If You Want To Conquer Fear, Do Not Sit Home And Think About It. Go Out And Get Busy.",
+		author: "Dale Breckenridge Carnegie"
+	},
+	{
+		quote: "The theory seems to be that as long as a man is a failure he is one of God's children, but that as soon as he succeeds he is taken over by the Devil.",
+		author: "H.L. Mencken",
+		profession: "American journalist and essayist"
 	}
-	// {
-	// 	quote: "",
-	// 	author: "",
-	// 	profession: ""
-	// }
 ]
 
 function newQuote() {
